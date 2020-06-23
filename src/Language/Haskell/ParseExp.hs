6c9a216d518f3d69cbf41fca648637b9218e67eb
Emil Axelsson <78emil@gmail.com>
2020-06-23 12:31:01 +0200

    Use Left instead of fail

--- src/Language/Haskell/ParseExp.hs
+++ src/Language/Haskell/ParseExp.hs
@@ -100,8 +100,8 @@ expPart first = do
           <++ list
           <++ literal first
           <++ tuple
-  -- Must handle lists before literals, because the "['a']" is accepted as a
-  -- String literal
+  -- Must handle lists before literals, because "['a']" is accepted as a string
+  -- literal
 
 -- | Expression parser
 expression :: ReadP Exp
@@ -117,13 +117,13 @@ expression = do
 parseExp :: String -> Either String Exp
 parseExp str = case [expr | (expr,"") <- readP_to_S expression str] of
     [expr] -> return expr
-    _ -> fail $ "parseExp: cannot parse '" ++ str ++ "'"
+    _ -> Left $ "parseExp: cannot parse '" ++ str ++ "'"
              ++ " (parseExp only supports a limited subset of Haskell)"
 
 -- | Parse a Haskell pattern (the supported subset is given above)
 parsePat :: String -> Either String Pat
 parsePat str = case [pat | (pat,"") <- readP_to_S name str] of
     [pat] -> return (VarP pat)
-    _ -> fail $ "parsePat: cannot parse '" ++ str ++ "'"
+    _ -> Left $ "parsePat: cannot parse '" ++ str ++ "'"
              ++ " (parsePat only supports a limited subset of Haskell)"
 
